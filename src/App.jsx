1b9fc5f8d38ad51cce6c0d08488986fba6e98f63
Santi1994r <santiagoruiz9416@gmail.com>
2023-03-01 22:18:53 -0300

    add navbar in the proyect

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import './App.css'
+import NavbarSR from './components/Navbar/NavbarSR'
+import About from './pages/About/About'
+import Contact from './pages/Contact/Contact'
+import Index from './pages/Index/Index'
+import Proyects from './pages/Proyects/Proyects'
 import RoutesPortfolio from './routes/RoutesPortfolio'
 
 function App() {
 
   return (
     <>
-      <RoutesPortfolio />
+      <NavbarSR />
+      <Index />
+      <About />
+      <Proyects />
+      <Contact />
+      {/* <RoutesPortfolio /> */}
     </>
   )
 }
--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -63,10 +63,10 @@ const Skills = () => {
     <section className='flex flex-col items-center pb-10'>
         <h5 className=' text-white text-center mt-28 mb-5 text-4xl font-bold'>Habilidades</h5>
 
-        <div className=' bg-indigo-900 w-auto h-44 sm:h-full sm:w-8/12 overflow-x-auto overscroll-x-contain flex p-5 items-center justify-center rounded-xl sm:overflow-hidden sm:flex-wrap'>
+        <div className=' bg-indigo-900 w-auto h-44 sm:h-full sm:w-8/12 overflow-x-auto overscroll-x-contain flex p-5 items-center justify-center rounded-xl sm:overflow-hidden sm:flex-wrap mx-4'>
 
           {
-            propsIcons.map((iconProp, index) => <IconSkill key={index} logoURL={iconProp.urlIcon} nameLogo={iconProp.nameAlt} widthLogo={iconProp.widthLogo} />)
+            propsIcons.map((iconProp, index) => <IconSkill className='px-5' key={index} logoURL={iconProp.urlIcon} nameLogo={iconProp.nameAlt} widthLogo={iconProp.widthLogo} />)
           }
 
         </div>
