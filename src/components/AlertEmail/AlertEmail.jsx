f375f7b18f1aaaa973d4b8f8f0a860cc1f844e28
Santi1994r <santiagoruiz9416@gmail.com>
2023-03-14 12:36:07 -0300

    remove close button in the alert

--- src/components/AlertEmail/AlertEmail.jsx
+++ src/components/AlertEmail/AlertEmail.jsx
@@ -10,28 +10,12 @@ const AlertEmail = ({icon, text1, text2}) => {
         <span className="text-green-600">
           {
             icon ? <HiOutlineCheckCircle className=' h-8 w-10 text-green-300' /> : <VscError className=" h-8 w-10 text-white" />
-          }
-          {/* <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth="1.5"
-            stroke="currentColor"
-            className="h-8 w-10"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-            />
-          </svg> */}
-          
+          }          
         </span>
 
         <div className="flex-1">
           <strong className="block font-medium text-white">
             {`${icon ? 'E-mail enviado con éxito' : 'Error'}`}
-            
           </strong>
 
           <p className="mt-1 text-sm text-white">
@@ -39,25 +23,6 @@ const AlertEmail = ({icon, text1, text2}) => {
             
           </p>
         </div>
-
-        <button className="text-white transition hover:text-gray-600">
-          <span className="sr-only">Dismiss popup</span>
-
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth="1.5"
-            stroke="currentColor"
-            className="h-6 w-6"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M6 18L18 6M6 6l12 12"
-            />
-          </svg>
-        </button>
       </div>
     </div>
   );
