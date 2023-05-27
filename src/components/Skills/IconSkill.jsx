82a3848d0d75d822972bc1e2ce69ef00c589bc33
Santi1994r <santiagoruiz9416@gmail.com>
2023-05-27 02:49:02 -0300

    add news logos

--- src/components/Skills/IconSkill.jsx
+++ src/components/Skills/IconSkill.jsx
@@ -1,7 +1,7 @@
 import anime from 'animejs/lib/anime.es.js';
 import { useEffect } from 'react';
 
-const IconSkill = ({logoURL, nameLogo, widthLogo, logo, urlText}) => {
+const IconSkill = ({logoURL, nameLogo, widthLogo}) => {
 
     const skillLogo = {
       targets: ".skillLogo",
@@ -14,51 +14,12 @@ const IconSkill = ({logoURL, nameLogo, widthLogo, logo, urlText}) => {
     useEffect(() => {
       anime(skillLogo)
       
-    }, [logo])
-    
-
-    /* const handleLogo = () => {
-      setLogoURL(!logo)
-    } */
+    }, [])
   return (
-    
     <>
-{/*     <img onMouseEnter={handleLogo} onMouseLeave={handleLogo} className={` ${widthLogo} m-2 hover:cursor-pointer skillLogo ${logo ? ' opacity-20' : null}`} src={logoURL} alt={nameLogo} /> */}
-    {
-      logo 
-      ? <img className={` ${widthLogo} m-2 skillLogo rounded-full`} src={urlText.urlText} alt={nameLogo} />
-      : <img className={` ${widthLogo} m-2 skillLogo`} src={logoURL} alt={nameLogo} />
-    }
+      <img className={` ${widthLogo} m-2 skillLogo`} src={logoURL} alt={nameLogo} />
     </>
   )
 }
-/* <span className=' text-white text-xl font-bold w-auto m-2 sm:w-32 text-center'>{nameLogo}</span> */
 
 export default IconSkill
-
-/*  import anime from 'animejs/lib/anime.es.js';
-import './skillIcon.css'
-
-
-const IconSkill = ({logoURL, nameLogo, widthLogo}) => {
-
-    const skillLogo = {
-      targets: ".skillLogo",
-      translateY: -15,
-      direction: "alternate",
-      easing: "easeInOutSine",
-      duration: 800,
-      loop: true
-    };
-    anime(skillLogo)
-    
-  return (
-    
-    <div className=' flex flex-col items-center'>
-        <img className={` ${widthLogo} m-2 hover:cursor-pointer skillLogo `} src={logoURL} alt={nameLogo} />
-        
-    </div>
-  )
-}
-
-export default IconSkill*/
--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,35 +1,34 @@
-import React, { useState } from 'react'
 import IconSkill from './IconSkill'
 
 const Skills = () => {
   const propsIcons = [
     {
-      urlIcon: 'https://i.imgur.com/F9Up8KU.png',
+      urlIcon: 'https://i.imgur.com/Y7f1PRX.png',
       nameAlt: 'HTML',
-      widthLogo: 'w-32'
+      widthLogo: 'w-36'
     },
     {
-      urlIcon: 'https://i.imgur.com/1lPbeVN.png',
+      urlIcon: 'https://i.imgur.com/tRnosUD.png',
       nameAlt: 'CSS',
       widthLogo: 'w-32'
     },
     {
-      urlIcon: 'https://i.imgur.com/yEmOWLP.png',
+      urlIcon: 'https://i.imgur.com/9ha7IEv.png',
       nameAlt: 'JAVASCRIPT',
       widthLogo: 'w-32'
     },
     {
-      urlIcon: 'https://i.imgur.com/4mSWRCz.png',
+      urlIcon: 'https://i.imgur.com/3BbmpvF.png',
       nameAlt: 'REACT JS',
       widthLogo: 'w-36'
     },
     {
-      urlIcon: 'https://i.imgur.com/eMRUOLh.png',
+      urlIcon: 'https://i.imgur.com/oiswPUu.png',
       nameAlt: 'SASS',
       widthLogo: 'w-32'
     },
     {
-      urlIcon: 'https://i.imgur.com/vucmtIm.png',
+      urlIcon: 'https://i.imgur.com/YMgCkGs.png',
       nameAlt: 'BOOTSTRAP',
       widthLogo: 'w-36'
     },
@@ -49,23 +48,17 @@ const Skills = () => {
       widthLogo: 'w-32'
     }
   ]
-  const [logo, setLogoURL] = useState(false)
-
-  const handleIcons = (childrenProp) => {
-    setLogoURL(childrenProp)
-    logo
-  }/* ${logo ? 'sm:w-52 w-auto' : 'sm:w-8/12'} */
   return (
     <section id='Skills' className='flex flex-col items-center pb-10'>
         <h5 className=' text-white text-center mt-28 mb-5 text-4xl font-bold'>Habilidades</h5>
-        <div className={` bg-indigo-900 h-56 sm:h-96 sm:w-8/12  overflow-x-auto overscroll-x-contain flex p-5 items-center sm:justify-center sm:items-end rounded-xl sm:overflow-hidden sm:flex-wrap mx-4`}>
+
+        <div className={` bg-indigo-900 h-56 sm:h-96 w-auto sm:w-8/12 overflow-x-auto overscroll-x-contain flex p-5 items-center sm:justify-center sm:items-end rounded-xl sm:overflow-hidden sm:flex-wrap mx-4`}>
 
           {
-            propsIcons.map((iconProp, index) => <IconSkill logo={logo} className='px-5' key={index} logoURL={iconProp.urlIcon} nameLogo={iconProp.nameAlt} widthLogo={iconProp.widthLogo} />)
+            propsIcons.map((iconProp, index) => <IconSkill className='px-5' key={index} logoURL={iconProp.urlIcon} nameLogo={iconProp.nameAlt} widthLogo={iconProp.widthLogo} />)
           }
 
         </div>
-          <button onClick={() => setLogoURL(!logo)} className=' bg-zinc-600 text-white rounded-lg p-1 hover:bg-zinc-500'>{logo ? 'Mostrar logos' : 'Mostrar nombres'}</button>
     </section>
   )
 }
