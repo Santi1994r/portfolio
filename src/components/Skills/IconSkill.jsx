8ef49cc352f45941e6428aacdff9a4838498f326
Santi1994r <santiagoruiz9416@gmail.com>
2023-06-02 17:36:03 -0300

    change color text in index component

--- src/components/Skills/IconSkill.jsx
+++ src/components/Skills/IconSkill.jsx
@@ -1,20 +1,6 @@
-/* import anime from 'animejs/lib/anime.es.js'; */
-import { useEffect } from 'react';
 
 const IconSkill = ({logoURL, nameLogo, widthLogo, key}) => {
 
-    /* const skillLogo = {
-      targets: ".skillLogo",
-      translateY: -30,
-      direction: "alternate",
-      easing: "easeInOutSine",
-      duration: 800,
-      loop: true
-    };
-    useEffect(() => {
-      anime(skillLogo)
-      
-    }, []) */
   return (
     <>
       <img key={key} className={` ${widthLogo} m-2`} src={logoURL} alt={nameLogo} />
--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -63,21 +63,6 @@ const Skills = () => {
       type: "cms",
     },
   ];
-/*   const iconDevelopment = propsIcons.filter(
-    (iconProp) => iconProp.type === "development"
-  );
-  const iconBookstore = propsIcons.filter(
-    (iconProp) => iconProp.type === "development"
-  );
-  const iconFramework = propsIcons.filter(
-    (iconProp) => iconProp.type === "development"
-  );
-  const iconControl = propsIcons.filter(
-    (iconProp) => iconProp.type === "development"
-  );
-  const iconCMS = propsIcons.filter(
-    (iconProp) => iconProp.type === "development"
-  ); */
   const handleFilter = (type) => {
     return propsIcons.filter(
       (iconProp) => iconProp.type === type
@@ -182,14 +167,3 @@ const Skills = () => {
 
 export default Skills;
 
-/* <section id='Skills' className='flex flex-col items-center pb-10'>
-        <h5 className=' text-white text-center mt-28 mb-5 text-4xl font-bold'>Habilidades</h5>
-
-        <div className={` bg-indigo-900 h-56 sm:h-auto w-auto sm:w-8/12 overflow-x-auto overscroll-x-contain flex p-5 items-center sm:justify-center sm:items-end rounded-xl sm:overflow-hidden sm:flex-wrap mx-4`}>
-
-          {
-            propsIcons.map((iconProp, index) => <IconSkill className='px-5' key={index} logoURL={iconProp.urlIcon} nameLogo={iconProp.nameAlt} widthLogo={iconProp.widthLogo} />)
-          }
-
-        </div>
-    </section> */
